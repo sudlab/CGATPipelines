e46f48f93de96c491569f0029e0cb5be8edc8900
Jaime A. Benayas <jaime.alvarez.benayas@gmail.com>
2015-12-07 19:16:52 +0000

    Changed support for archive versions of ensembl (will not work with new ones, don't merge back)

--- CGATPipelines/pipeline_annotations.py
+++ CGATPipelines/pipeline_annotations.py
@@ -1182,8 +1182,7 @@ def downloadTranscriptInformation(infile, outfile):
         "source": "source",
         "status": "gene_status",
         "transcript_status": "transcript_status",
-        "external_gene_id": "gene_name",
-        "transcript_tsl": "transcript_support"
+        "external_gene_id": "gene_name"
     }
 
     data = Biomart.biomart_iterator(
